82a5db23f900029693f102f5d835f19411907127
RenataAltFo <a22100218@ceti.mx>
2025-09-12 13:56:17 -0600

    menu y perfil

---
+++ galileo/menu.gd
@@ -0,0 +1,10 @@
+extends MenuBar
+
+func _on_perfil_pressed():
+	get_tree().change_scene_to_file("res://scenes/ui/usuario/profile.tscn")
+
+func _on_leccion_pressed():
+	get_tree().change_scene_to_file("res://scenes/ui/usuario/LeccionesInicio.tscn")
+
+func _on_estadistica_pressed():
+	get_tree().change_scene_to_file("res://scenes/ui/usuario/estadisticas.tscn") # asegúrate de poner el archivo .tscn correcto
