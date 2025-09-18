6f2e3d754305e1bb962c78c8da41f1304c639840
TonyCast2005 <a21300618@ceti.mx>
2025-09-17 22:54:03 -0600

    prueba 1

--- galileo/escenas/usuario/registro/registrarse.gd
+++ galileo/escenas/usuario/registro/registrarse.gd
@@ -2,5 +2,5 @@ extends Control
 var change_scene = preload("res://escenas/usuario/registro/iniciarSesion.tscn")
 
 func _on_inicias_sesion_pressed() -> void:
-	get_tree().change_scene_to_packed(change_scene)
-	pass # Replace with function body.
+    get_tree().change_scene_to_packed(change_scene)
+    pass # Replace with function body.
