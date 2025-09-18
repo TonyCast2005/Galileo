b930da1db3c02b4d5d329c0a8c41ecfe09f4c4c4
TonyCast2005 <a21300618@ceti.mx>
2025-09-18 08:13:43 -0600

    algunas escenas creadas

--- galileo/escenas/usuario/registro/registrarse.gd
+++ galileo/escenas/usuario/registro/registrarse.gd
@@ -4,3 +4,7 @@ var change_scene = preload("res://escenas/usuario/registro/iniciarSesion.tscn")
 func _on_inicias_sesion_pressed() -> void:
     get_tree().change_scene_to_packed(change_scene)
     pass # Replace with function body.
+
+
+func _on_aceptar_pressed() -> void:
+    pass # Replace with function body.
