9633bd7515c42ea7554678f029adda7c90be6d81
TonyCast2005 <a21300618@ceti.mx>
2025-09-28 22:22:46 -0600

    Terminado

--- galileo/Scripts/TestUbicacion/AvisoDeNivel/aviso_nivel.gd
+++ galileo/Scripts/TestUbicacion/AvisoDeNivel/aviso_nivel.gd
@@ -1,3 +1,18 @@
 extends Control
-func lanzar_confetti():
-    $Confetti.emitting = true
+
+@onready var label_nivel = $LabelNivel
+
+
+func _ready():
+    var resultado = Global.get("resultado_examen")
+    var puntaje = resultado["puntaje"]
+    var nivel = resultado["nivel"]
+
+    label_nivel.text =  nivel
+   
+
+
+func _on_continuar_pressed():
+            get_tree().change_scene_to_file("res://escenas/usuario/Perfil/perfil.tscn")
+
+    
