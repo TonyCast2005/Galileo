cd5bc7fc867b5439afcefacc58f31a582ce3929b
TonyCast2005 <a21300618@ceti.mx>
2025-10-01 20:25:22 -0600

    No terminado

--- galileo/escenas/usuario/Perfil/perfil.gd
+++ galileo/escenas/usuario/Perfil/perfil.gd
@@ -1,14 +1,49 @@
 extends Control
 
-@onready var achievements_list = $ScrollContainer # mejor que Control sea un VBoxContainer
-var LogroScene = preload("res://escenas/usuario/Perfil/Logro.tscn")  # cambia a la ruta de tu escena de logro
+@onready var achievements_list = $ScrollContainer/logrosVbox
+@onready var http = $HTTPRequest
 
-func add_achievement(icon: Texture, title: String, description: String):
-	var logro = LogroScene.instantiate()
-	logro.set_data(icon, title, description)
-	achievements_list.add_child(logro)
+var LogroScene = preload("res://escenas/usuario/Perfil/Logro.tscn")
+var firebase_url = "https://galileo-af640-default-rtdb.firebaseio.com/" 
+
+var logros = {}
 
 func _ready():
-	var icon = preload("res://assets/sprites/ui/Logros/de noche todos los gatos son pardos.png")  # cámbialo a tu icono
-	add_achievement(icon, "Primer paso", "Completaste el tutorial con éxito")
-	add_achievement(icon, "Explorador", "Visitaste todas las secciones del perfil")
+    # Pedimos los logros globales
+    var url_logros = "%s/logros.json" % firebase_url
+    http.request(url_logros)
+
+# Este método recibe TODAS las respuestas de HTTPRequest
+func _on_HTTPRequest_request_completed(result, response_code, headers, body):
+    if response_code != 200:
+        push_error("Error al cargar Firebase: %s" % response_code)
+        return
+
+    var data = {}
+    if body.size() > 0:
+        data = JSON.parse_string(body.get_string_from_utf8())
+    
+    if data == null:
+        push_error("Error al parsear JSON")
+        return
+
+    # Guardamos los logros
+    logros = data
+
+    # Mostramos todos los logros como desbloqueados
+    mostrar_logros()
+
+func mostrar_logros():
+    # Limpiamos la lista antes de recargar
+    for child in achievements_list.get_children():
+        child.queue_free()
+
+    for id in logros.keys():
+        var data = logros[id]
+        var icon = load(data["icono"]) # carga la textura desde la ruta
+        add_achievement(icon, data["nombre"], data["descripcion"], true) # desbloqueado por defecto
+
+func add_achievement(icon: Texture, title: String, description: String, unlocked: bool):
+    var logro = LogroScene.instantiate()
+    achievements_list.add_child(logro)
+    logro.call_deferred("set_data", icon, title, description, unlocked)
