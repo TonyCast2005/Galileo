34677ac30d56a48eb88c09ddd5d2b7db9e1d2e6e
RenataAltFo <a22100218@ceti.mx>
2025-10-01 21:38:54 -0600

    menu

---
+++ galileo/escenas/TestUbicacion/test_1.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@onready var novato = $ColorRect/Novato
+@onready var raiz = $ControlGato
+@onready var competente = $ColorRect/Competente
+@onready var experimentado = $ColorRect/Experimentado
+
+var instancia_actual
+
+func _on_button_pressed():
+	var escena = preload("res://escenas/TestUbicacion/explicacion.tscn").instantiate()
+	raiz.add_child(escena)
+	instancia_actual = escena
+
+func _on_competente_pressed():
+	pass
+
+func _on_experimentado_pressed():
+	pass
