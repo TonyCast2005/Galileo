f7acaaf8c51aab4de42c46b62d45e6a710dfc25f
TonyCast2005 <a21300618@ceti.mx>
2025-11-20 01:34:40 -0600

    r

--- galileo/escenas/usuario/Perfil/perfil.gd
+++ galileo/escenas/usuario/Perfil/perfil.gd
@@ -5,29 +5,29 @@ extends Control
 @onready var profile_pic = $FotoPerfil      # TextureRect de la foto
 
 func _ready():
-	cargar_datos_usuario()
+    cargar_datos_usuario()
 
 func cargar_datos_usuario():
-	var user = Globals.user
+    var user = Globals.user
 
-	# Nombre
-	username.text = user.get("nombre", "Usuario sin nombre")
+    # Nombre
+    username.text = user.get("nombre", "Usuario sin nombre")
 
-	# Nivel
-	level_label.text = user.get("nivel", "novato")
+    # Nivel
+    level_label.text = user.get("nivel", "novato")
 
-	# Foto de perfil
-	var foto_id = user.get("foto", "default")  # <-- esto ya tiene el valor guardado en Globals.user
+    # Foto de perfil
+    var foto_id = user.get("foto", "default")  # <-- esto ya tiene el valor guardado en Globals.user
 
-	# Ruta local de la imagen
-	var ruta = "res://assets/sprites/trophies/%s" % foto_id
+    # Ruta local de la imagen
+    var ruta = "res://assets/sprites/trophies/%s" % foto_id
 
-	if ResourceLoader.exists(ruta):
-		profile_pic.texture = load(ruta)
-	else:
-		# Si no existe el archivo, usar la imagen por defecto
-		profile_pic.texture = load("res://assets/sprites/ui/Logros/el minino resiste.png")
+    if ResourceLoader.exists(ruta):
+        profile_pic.texture = load(ruta)
+    else:
+        # Si no existe el archivo, usar la imagen por defecto
+        profile_pic.texture = load("res://assets/sprites/ui/Logros/el minino resiste.png")
 
 
 func _on_editar_perfil_pressed():
-	get_tree().change_scene_to_file("res://escenas/usuario/Perfil/EditarPerfil.tscn")
+    get_tree().change_scene_to_file("res://escenas/usuario/Perfil/EditarPerfil.tscn")
