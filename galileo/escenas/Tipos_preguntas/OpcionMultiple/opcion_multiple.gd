f58292788f4d4bb6d7860be06221e1387ae4ec6d
TonyCast2005 <a21300618@ceti.mx>
2025-11-20 09:47:28 -0600

    k

--- galileo/escenas/Tipos_preguntas/OpcionMultiple/opcion_multiple.gd
+++ galileo/escenas/Tipos_preguntas/OpcionMultiple/opcion_multiple.gd
@@ -11,45 +11,45 @@ var indice_actual = 0
 const URL = "https://galileo-af640-default-rtdb.firebaseio.com/preguntas_opc.json"
 
 func _ready():
-	cargar_preguntas()
+    cargar_preguntas()
 
 func cargar_preguntas():
-	var http := HTTPRequest.new()
-	add_child(http)
-	http.request_completed.connect(_on_request_completed)
-	http.request(URL)
+    var http := HTTPRequest.new()
+    add_child(http)
+    http.request_completed.connect(_on_request_completed)
+    http.request(URL)
 
 func _on_request_completed(result, response_code, headers, body):
-	var data = JSON.parse_string(body.get_string_from_utf8())
+    var data = JSON.parse_string(body.get_string_from_utf8())
 
-	for id in data.keys():
-		preguntas.append(data[id])
+    for id in data.keys():
+        preguntas.append(data[id])
 
-	mostrar_pregunta(indice_actual)
+    mostrar_pregunta(indice_actual)
 
 
 func mostrar_pregunta(i):
-	var p = preguntas[i]
+    var p = preguntas[i]
 
-	label_pregunta.text = p["pregunta"]
+    label_pregunta.text = p["pregunta"]
 
-	var opciones = p["opciones"]
-	var correcta = opciones[p["correcta"] - 1]
+    var opciones = p["opciones"]
+    var correcta = opciones[p["correcta"] - 1]
 
-	boton1.text = opciones[0]
-	boton2.text = opciones[1]
-	boton3.text = opciones[2]
+    boton1.text = opciones[0]
+    boton2.text = opciones[1]
+    boton3.text = opciones[2]
 
-	for b in [boton1, boton2, boton3]:
-		for s in b.pressed.get_connections():
-			b.pressed.disconnect(s["callable"])
+    for b in [boton1, boton2, boton3]:
+        for s in b.pressed.get_connections():
+            b.pressed.disconnect(s["callable"])
 
-	boton1.pressed.connect(func(): responder(opciones[0], correcta))
-	boton2.pressed.connect(func(): responder(opciones[1], correcta))
-	boton3.pressed.connect(func(): responder(opciones[2], correcta))
+    boton1.pressed.connect(func(): responder(opciones[0], correcta))
+    boton2.pressed.connect(func(): responder(opciones[1], correcta))
+    boton3.pressed.connect(func(): responder(opciones[2], correcta))
 
 func responder(r, correcta):
-	if r == correcta:
-		print("Correcta!")
-	else:
-		print("Incorrecta!")
+    if r == correcta:
+        print("Correcta!")
+    else:
+        print("Incorrecta!")
