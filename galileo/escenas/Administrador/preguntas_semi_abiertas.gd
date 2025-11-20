1d2bd642f51d68d55e858e4ee370f6ce1df39c99
RenataAltFo <a22100218@ceti.mx>
2025-11-20 00:59:10 -0600

    respaldo

--- galileo/escenas/Administrador/preguntas_semi_abiertas.gd
+++ galileo/escenas/Administrador/preguntas_semi_abiertas.gd
@@ -1,4 +1,23 @@
 extends Control
+@onready var pregunta = $pregunta
+@onready var subpregunta = $subpregunta
+@onready var respuesta = $respuesta
+@onready var mensaje = $Mensaje
 
 func _on_volver_pressed():
 	get_tree().change_scene_to_file("res://escenas/Administrador/AgregarPregunta.tscn")
+
+func _on_previsualizar_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_eliminar_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_borrador_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_guardar_pressed() -> void:
+	pass # Replace with function body.
